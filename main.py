9d1682278556c477089e74142ce489ca2c1a48c5
Anderson Silva <anderson.bhilly@gmail.com>
2022-10-25 06:01:18 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,37 @@
+from PyQt5 import uic,QtWidgets
+
+def chama_tela_login():
+    TelaLogin.show()
+    TelaPrincipal.close()
+    print('Apareceu a tela!')
+
+def chama_tela_usuario():
+    TelaUsuario.show()
+    TelaPrincipal.close()
+
+def chama_tela_principal():
+    TelaPrincipal.show()
+    TelaUsuario.close()
+    TelaPessoa.close()
+
+def chama_tela_pessoa():
+    TelaPessoa.show()
+    TelaPrincipal.close()
+
+app=QtWidgets.QApplication([])
+
+TelaLogin=uic.loadUi('TelaLogin.ui')
+TelaPrincipal=uic.loadUi('TelaPrincipal.ui')
+TelaUsuario=uic.loadUi('TelaUsuario.ui')
+TelaPessoa=uic.loadUi('TelaPessoa.ui')
+
+TelaPrincipal.btnCadUser.clicked.connect(chama_tela_usuario)
+TelaPrincipal.btnCadPessoa.clicked.connect(chama_tela_pessoa)
+TelaPrincipal.btnVoltar.clicked.connect(chama_tela_login)
+
+TelaUsuario.btnVoltar.clicked.connect(chama_tela_principal)
+
+TelaPessoa.btnVoltar.clicked.connect(chama_tela_principal)
+
+TelaPrincipal.show()
+app.exec()
