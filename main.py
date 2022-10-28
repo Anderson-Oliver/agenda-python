45cd574a2ee199c530c5bc544249a4f97c6949e0
Anderson Silva <anderson.bhilly@gmail.com>
2022-10-28 10:24:46 -0300

    Deletando o arquivo funcao

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from PyQt5 import uic,QtWidgets
 
 
+def chama_tela_login():
+    TelaLogin.show()
+    TelaPrincipal.close()
+
+def chama_tela_usuario():
+    TelaUsuario.show()
+    TelaPrincipal.close()
+
+def chama_tela_principal():
+    TelaPrincipal.show()
+    TelaUsuario.close()
+    TelaPessoa.close()
+
+def chama_tela_pessoa():
+    TelaPessoa.show()
+    TelaPrincipal.close()
 
 app=QtWidgets.QApplication([])
 
